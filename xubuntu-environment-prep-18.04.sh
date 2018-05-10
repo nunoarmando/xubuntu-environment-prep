b78948f53ee8c68b404f1e70f61f7f1ca5182135
Nuno Serrano <nunoarmando@gmail.com>
2018-05-10 18:18:36 +0100

    Update xubuntu-environment-prep-18.04.sh

--- xubuntu-environment-prep-18.04.sh
+++ xubuntu-environment-prep-18.04.sh
@@ -89,8 +89,6 @@ curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
 
 
-
-
 ###############################################################
 # Update system
 ###############################################################
