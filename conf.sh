aac5006da3e05f202fe7395424fb10d2f04c9ec2
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 22:36:40 +0100

    cp

--- conf.sh
+++ conf.sh
@@ -2,11 +2,6 @@
 # User Configurations
 ###############################################################
 
-sudo cp -rfv ./cfg/* ~/.config/
-sudo mkdir ./cfg_dockbarx/
-sudo cp -rfv ./cfg_dockbarx/* ~/.dockbarx/
-sudo cp -rfv ./Templates/* ~/Templates/
-sudo cp -rfv ./Pictures/* ~/Pictures/
-sudo cp -rfv ./local_share/* ~/.local/share/
+sudo cp -rfv ./user/* ~/
 sudo cp -rfv ./etc/* /etc/
 
