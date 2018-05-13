112721c281ba1d9256dab8c9e2a62d1f2511a527
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 23:07:33 +0100

    cp a

--- conf.sh
+++ conf.sh
@@ -2,6 +2,9 @@
 # User Configurations
 ###############################################################
 
-sudo cp -rfv ./user/ ~/
-sudo cp -rfv ./etc/ /etc/
+# sudo cp -rfv ./user/ ~/
+# sudo cp -rfv ./etc/ /etc/
+
+sudo cp -afv ./user/. ~/
+sudo cp -afv ./etc/. /etc/
 
