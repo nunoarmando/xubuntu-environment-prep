c9af121397156043b988ff8ec7b7df407fc4bc8c
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 20:54:26 +0100

    conf

--- subuntu-test.sh
+++ subuntu-test.sh
@@ -89,28 +89,28 @@ sudo apt-get update
 # User Configurations
 ###############################################################
 
-sleep 1
-echo '############## 5 #######################'
-sleep 1
-echo '############## 4 #######################'
-sleep 1
-echo '############## 3 #######################'
-sleep 1
-echo '############## 2 #######################'
-sleep 1
-echo '############## 1 #######################'
-sleep 1
-
-xfce4-panel --quit && pkill xfconfd
-
-sudo cp -rfv ./cfg/* ~/.config/
-sudo mkdir ./cfg_dockbarx/
-sudo cp -rfv ./cfg_dockbarx/* ~/.dockbarx/
-sudo cp -rfv ./Templates/* ~/Templates/
-sudo cp -rfv ./Pictures/* ~/Pictures/
-sudo cp -rfv ./local_share/* ~/.local/share/
-sudo cp -rfv ./etc/* /etc/
-
+#sleep 1
+#echo '############## 5 #######################'
+#sleep 1
+#echo '############## 4 #######################'
+#sleep 1
+#echo '############## 3 #######################'
+#sleep 1
+#echo '############## 2 #######################'
+#sleep 1
+#echo '############## 1 #######################'
+#sleep 1
+#
+#xfce4-panel --quit && pkill xfconfd
+#
+#sudo cp -rfv ./cfg/* ~/.config/
+#sudo mkdir ./cfg_dockbarx/
+#sudo cp -rfv ./cfg_dockbarx/* ~/.dockbarx/
+#sudo cp -rfv ./Templates/* ~/Templates/
+#sudo cp -rfv ./Pictures/* ~/Pictures/
+#sudo cp -rfv ./local_share/* ~/.local/share/
+#sudo cp -rfv ./etc/* /etc/
+#
 #xfce4-panel
 
 ###############################################################
