17d9e4021d13f69ce8c9a611116a0395117bce8b
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 16:45:23 +0100

    Background

--- subuntu-test.sh
+++ subuntu-test.sh
@@ -93,6 +93,7 @@ sudo cp -rfv ./cfg/* ~/.config/
 sudo mkdir ./cfg_dockbarx/
 sudo cp -rfv ./cfg_dockbarx/* ~/.dockbarx/
 sudo cp -rfv ./Templates/* ~/Templates/
+sudo cp -rfv ./Pictures/* ~/Pictures/
 sudo cp -rfv ./local_share/* ~/.local/share/
 sudo cp -rfv ./etc/* /etc/
 
