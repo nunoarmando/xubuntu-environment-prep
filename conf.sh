95770de18fdb440e90eb06f1c59354160ecbf65c
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 23:29:22 +0100

    delete git folder

--- conf.sh
+++ conf.sh
@@ -2,9 +2,9 @@
 # User Configurations
 ###############################################################
 
-# sudo cp -rfv ./user/ ~/
-# sudo cp -rfv ./etc/ /etc/
-
 sudo cp -afv ./user/. ~/
 sudo cp -afv ./etc/. /etc/
+
+cd ..
+rm -Rf ./naas_x_prep
 
