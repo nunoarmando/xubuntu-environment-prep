fe677d9eafd71dc3e6c7a277f870aee1855362ce
Nuno Serrano <nunoarmando@gmail.com>
2018-05-13 21:04:34 +0100

    conf files

--- conf.sh
+++ conf.sh
@@ -2,20 +2,6 @@
 # User Configurations
 ###############################################################
 
-sleep 1
-echo '############## 5 #######################'
-sleep 1
-echo '############## 4 #######################'
-sleep 1
-echo '############## 3 #######################'
-sleep 1
-echo '############## 2 #######################'
-sleep 1
-echo '############## 1 #######################'
-sleep 1
-
-xfce4-panel --quit && pkill xfconfd
-
 sudo cp -rfv ./cfg/* ~/.config/
 sudo mkdir ./cfg_dockbarx/
 sudo cp -rfv ./cfg_dockbarx/* ~/.dockbarx/
@@ -23,6 +9,3 @@ sudo cp -rfv ./Templates/* ~/Templates/
 sudo cp -rfv ./Pictures/* ~/Pictures/
 sudo cp -rfv ./local_share/* ~/.local/share/
 sudo cp -rfv ./etc/* /etc/
-
-#xfce4-panel
-
